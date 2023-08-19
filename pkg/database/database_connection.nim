6582cd3566520ba19c1854ee5ef80199b83cf37a
wurfkreuz <wurfkreuz@mail.ru>
2023-08-19 15:20:42 +0300

    n

--- pkg/database/database_connection.nim
+++ pkg/database/database_connection.nim
@@ -1,4 +1,21 @@
-import db_connector/db_postgres, ../../src/config
+import db_connector/db_postgres
 
-let conn = open(dbHost, dbName, dbUser, dbPassword)
-conn.close()
+
+proc addMessage*(conn: Dbconn, text: string) =
+  let sqlQuery = sql"INSERT INTO messages(text) VALUES(?)"  # It takes a parameter against the query, that's why Dbconn parameter is ignored. It's not a part of it.
+  conn.exec(sqlQuery, text)
+
+
+proc getMessages*(conn: Dbconn) =
+  let rows = conn.getAllRows(sql"SELECT text FROM messages")
+  for row in rows:
+    echo row
+
+proc testConnection*(conn: Dbconn): bool =
+  try:
+    let queryResult = conn.getAllRows(sql"SELECT 1")  # Adjust this line based on the actual method for executing queries
+    if queryResult.len > 0 and queryResult[0][0] == "1":
+      return true
+  except Exception:
+    echo "An error occurred while testing the connection: ", getCurrentExceptionMsg()
+  return false
--- src/flashcards_bot.nim
+++ src/flashcards_bot.nim
@@ -1,23 +1,47 @@
-import httpclient, json, os, ../pkg/message_functions/reply
+import httpclient, json, os, ../pkg/message_functions/reply, ../src/config, db_connector/db_postgres, ../pkg/database/database_connection
 
-let botToken = getEnv("TG_API_TOKEN")
-let client = newHttpClient()  # Create a new HTTP client
-var offset = 0
+proc main() =
+
+  let conn = open(dbHost, dbUser, dbPassword, dbName)
+  # defer: conn.close()  # The connection will be closed when exiting the current scope.
+  
+  let botToken = getEnv("TG_API_TOKEN")
+  let client = newHttpClient()  # Create a new HTTP client
+  var offset = 0
+
+  if testConnection(conn):
+    echo "Connection is working!"
+  else:
+    echo "Connection failed!"
+  
+  
+  while true:
+    let url = "https://api.telegram.org/bot" & botToken & "/getUpdates?offset=" & $offset
+    let response = client.getContent(url)  # Send the request
+    let updates = parseJson(response)  # Parse the JSON response
+    echo "This is offset in the start of the loop: " & $offset
+  
+    for update in updates["result"]:
+      let updateId = update["update_id"].getInt
+      echo "This is updateId: " & $updateId
+      let chatId = update["message"]["chat"]["id"].getInt
+      if "text" in update["message"]:
+        try:
+          let incomingMessage = update["message"]["text"].getStr
+          if incomingMessage == "add" and not update["message"]["from"]["is_bot"].getBool:
+            reply.simpleResponse(chatId, "Added!")
+            echo "This is incomingMessage: " & incomingMessage
+            addMessage(conn, incomingMessage)
+          elif incomingMessage == "/list":
+            getMessages(conn)
+        except Exception:
+          echo getCurrentExceptionMsg()
+      # if update["message"]["from"]["is_bot"].getBool:
+      #   offset = updateId + 1
+      # else:
+      offset = updateId + 1
+      echo "This is offset in the end of the loop: " & $offset
 
 
-while true:
-  let url = "https://api.telegram.org/bot" & botToken & "/getUpdates?offset=" & $offset
-  let response = client.getContent(url)  # Send the request
-  let updates = parseJson(response)  # Parse the JSON response
+main()
 
-  for update in updates["result"]:
-    let updateId = update["update_id"].getInt
-    let chatId = update["message"]["chat"]["id"].getInt
-    if "text" in update["message"]:
-      try:
-        let incomingMessage = update["message"]["text"].getStr
-        if incomingMessage == "/add" and not update["message"]["from"]["is_bot"].getBool:
-          reply.simpleResponse(chatId, "Added!")
-      except:
-        continue
-    offset = updateId + 1
